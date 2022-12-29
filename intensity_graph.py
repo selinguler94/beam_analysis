2c7f27f7a1d3439781f305161edbb95da6ca99a4
selinguler94 <81233325+selinguler94@users.noreply.github.com>
2022-12-29 15:03:11 +0300

    Update intensity_graph.py

--- intensity_graph.py
+++ intensity_graph.py
@@ -4,17 +4,22 @@ import numpy as np
 from PIL import Image, ImageFilter
 
 image = Image.open(r'')
-#median filter is optional 
+#filter is optional 
 image = image.filter(ImageFilter.MedianFilter(3))
 pixels = image.load()
+
 x_axis, y_axis, z_axis = [], [], []
+
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
+
+#intensity distrubution of the 'image'
 for x in range(image.width):
     for y in range(image.height):
         x_axis.append(x)
         y_axis.append(y)
         z_axis.append(sum(pixels[x, y]))
+        
 ax.scatter(x_axis, y_axis, z_axis, c=z_axis, cmap=cm.jet)
 fig.colorbar(ax.scatter(x_axis, y_axis, z_axis, c=z_axis, cmap=cm.jet))
 plt.show()
