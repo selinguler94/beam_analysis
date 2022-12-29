080e4752a0387f66b1b94cc23829747a97006172
selinguler94 <81233325+selinguler94@users.noreply.github.com>
2022-12-29 16:39:46 +0300

    Update beam_analysis.py

--- beam_analysis.py
+++ beam_analysis.py
@@ -2,16 +2,20 @@ from PIL import Image, ImageFilter
 import matplotlib.pyplot as plt
 import math
 
+
 image = Image.open(r'')
 image = image.filter(ImageFilter.MedianFilter(3))
 pixels = image.load()
 
+
 y_axis = int(image.height/2)
 irradiance, section = [], []
+
 
 for i in range(image.width):
     irradiance.append((sum(pixels[i, y_axis])))
     section.append((i + 1))
+
     
 plt.plot(section, irradiance)
 plt.ylabel('irradiance (W/m^2)'), plt.xlabel('distance (m)')
