358e6e852fccdea06a8b6f7a17431157c9e039ea
selinguler94 <81233325+selinguler94@users.noreply.github.com>
2022-12-29 20:54:37 +0300

    Update intensity_graph_trial.py

--- intensity_graph_trial.py
+++ intensity_graph_trial.py
@@ -13,12 +13,15 @@ distance = np.arange(0, 60, 10)
 #Beam radius (m) using the equation for a Gaussian beam
 beam_radius = wavelength * distance / (np.pi * np.sqrt(2))
 
+
 #Irradiance (W/m^2) using the equation for a Gaussian beam
 irradiance = ((power / (np.pi * beam_radius**2)) * np.exp(-2 * (distance / beam_radius)**2))
+
 
 #error: the peak irradiance valued as 'nan W/m^2'
 beam_radius[beam_radius <= 0] = 1e-10
 distance[distance == 0] = 1e-10
+
 
 #irradiance as a function of distance
 plt.plot(distance, irradiance)
@@ -26,9 +29,11 @@ plt.xlabel('Distance (m)')
 plt.ylabel('Irradiance (W/m^2)')
 plt.title('Irradiance distance graph of a Gaussian beam')
 
+
 #peak irradiance
 peak_irradiance = np.max(irradiance)
 print('Peak irradiance:', peak_irradiance, 'W/m^2')
+
 
 #calculating the diameter beam_diameter = beam_radius
 e1_irradiance = peak_irradiance * np.e
